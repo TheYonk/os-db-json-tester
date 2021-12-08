89698bd8509bd1e7d0cd45dfa1ab3f6b4a6fa6ab
The Yonk - Matt Yonkovit <matt@theyonk.com>
2021-12-08 11:11:28 -0800

    Counts per second calced incorrectly...

--- bench/app_controller.py
+++ bench/app_controller.py
@@ -384,7 +384,7 @@ try:
             logging.info('Active Counts: %s',countlist )
             logging.info('Timing Counts: %s',timinglist )
             res = [round((i / j),2) if j != 0 else 0 for i, j in zip(timinglist, countlist)]
-            cntper = [round((i / 30),2) for i in countlist]
+            cntper = [round((i / xt),2) for i in countlist]
             logging.info('Time Per: %s',res )
             logging.info('Count PS: %s',cntper )
             
@@ -394,7 +394,7 @@ try:
             logging.info('FINAL: Active Counts: %s',countlist )
             logging.info('FINAL: Timing Counts: %s',timinglist )
             res = [round((i / j),2) if j != 0 else 0 for i, j in zip(timinglist, countlist)]
-            cntper = [round((i / 30),2) for i in countlist]
+            cntper = [round((i / xt),2) for i in countlist]
             logging.info('FINAL: Time Per: %s',res )
             logging.info('FINAL: Count PS: %s',cntper )
         
